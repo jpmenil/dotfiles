ad6533fd2d4a08ccf437817c32943dfe4b229b72
Jean-Philippe Menil <jpmenil@gmail.com>
2020-05-16 17:35:53 +0200

    zsh: fix grep colors, update ls ones
    Signed-off-by: Jean-Philippe Menil <jpmenil@gmail.com>

--- .config/zsh/rc/01-export.zsh
+++ .config/zsh/rc/01-export.zsh
@@ -1,12 +1,12 @@
 # -*- sh -*-
 
-export LS_COLORS="di=0;35:ln=1;31:so=37:pi=1;35:ex=37:bd=37:cd=37:su=37:sg=37:tw=1;36:ow=1;36"
+export LS_COLORS="di=1;36:ln=1;31:so=37:pi=1;35:ex=37:bd=37:cd=37:su=37:sg=37:tw=1;36:ow=1;36"
 export XDG_CONFIG_HOME="$HOME/.config"
 export GOPATH="$HOME/go"
 
 export VISUAL="qe"
 export EDITOR=$VISUAL
-export GREP_COLOR='1;32'
+export GREP_COLORS='ms=01;32:mc=01;31:sl=:cx=:fn=33:ln=32:bn=32:se=36'
 
 export GPG_TTY=$(tty)
 export SSH_AUTH_SOCK="${HOME}/.gnupg/S.gpg-agent.ssh"
