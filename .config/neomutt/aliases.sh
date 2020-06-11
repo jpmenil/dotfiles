1fdce911682bf6af18385ca45be961e67909d42c
Jean-Philippe Menil <jpmenil@gmail.com>
2020-06-11 10:56:23 +0200

    neomutt: source alias
    Signed-off-by: Jean-Philippe Menil <jpmenil@gmail.com>

--- .config/neomutt/aliases.sh
+++ .config/neomutt/aliases.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ALIASFILE="$HOME/.config/neomutt/aliases.txt"
+ALIASFILE="$HOME/.config/neomutt/aliases"
 MESSAGE=$(cat)
 
 NEWALIAS=$(echo "${MESSAGE}" | grep ^"From: " | sed s/[\,\"\']//g | awk '{$1=""; if (NF == 3) {print "alias" $0;} else if (NF == 2) {print "alias" $0 $0;} else if (NF > 3) {print "alias", tolower($(NF-1))"-"tolower($2) $0;}}')
