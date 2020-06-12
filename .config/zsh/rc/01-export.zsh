c0339a3e83a33fde6a18e4135dbbc3b8764d0bc6
Jean-Philippe Menil <jpmenil@gmail.com>
2020-06-12 08:24:14 +0200

    set pager to less
    Signed-off-by: Jean-Philippe Menil <jpmenil@gmail.com>

--- .config/zsh/rc/01-export.zsh
+++ .config/zsh/rc/01-export.zsh
@@ -1,13 +1,14 @@
 # -*- sh -*-
 
-export LS_COLORS="di=1;36:ln=1;31:so=37:pi=1;35:ex=37:bd=37:cd=37:su=37:sg=37:tw=1;36:ow=1;36"
-export XDG_CONFIG_HOME="$HOME/.config"
 export GOPATH="$HOME/go"
-
+export GREP_COLORS='ms=01;32:mc=01;31:sl=:cx=:fn=33:ln=32:bn=32:se=36'
+export LS_COLORS="di=1;36:ln=1;31:so=37:pi=1;35:ex=37:bd=37:cd=37:su=37:sg=37:tw=1;36:ow=1;36"
+export PAGER="less"
 export VISUAL="qe"
 export EDITOR=$VISUAL
-export GREP_COLORS='ms=01;32:mc=01;31:sl=:cx=:fn=33:ln=32:bn=32:se=36'
+export XDG_CONFIG_HOME="$HOME/.config"
 
+# gpg & ssh
 export GPG_TTY=$(tty)
 export SSH_AUTH_SOCK="${HOME}/.gnupg/S.gpg-agent.ssh"
 gpg-connect-agent -q updatestartuptty /bye >> /dev/null
