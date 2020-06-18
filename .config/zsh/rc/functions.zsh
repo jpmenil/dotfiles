b1e661ccefad1dbff4df292c6cc8f53366d4c149
Jean-Philippe Menil <jpmenil@gmail.com>
2020-06-18 21:17:18 +0200

    zsh: bit of rewaork, lot of cleanup
    Signed-off-by: Jean-Philippe Menil <jpmenil@gmail.com>

--- .config/zsh/rc/functions.zsh
+++ .config/zsh/rc/functions.zsh
@@ -2,42 +2,12 @@
 
 # Functions
 
-# MOTD
-function echo_color() {
-    local color="$1"
-    #printf "${color}$2\033[0m\n"
-    printf "${color}$2\n"
-}
-#echo_color "\033[0;33m" "c-b Move backward     c-f Move forward"
-#echo_color "\033[0;33m" "c-p Move up           c-n Move down"
-#echo_color "\033[0;33m" "c-a Beginning of line c-e End of line"
-#echo_color "\033[0;33m" "c-d Delete forward    c-h Delete backward"
-#echo_color "\033[0;33m" "c-k Delete forward to end of line"
-#echo_color "\033[0;33m" "c-u Delete entire line"
-#echo_color "\033[0;33m" "a-b Word to left       a-f Word to right"
-
 function mfa () {
     oathtool --base32 --totp "$(pass show $1/2fa)" | xclip -selection clipboard ;
 }
 
-#function hostname_ssh () {
-#	tty=${1:-$(tmux display -p '#{pane_tty}')}
-#	ssh_parameters=$(ps -t "$tty" -o command= | awk '/ssh/ && !/-W/ { $1=""; print $0; exit }')
-#	hostname=$(ssh -G $ssh_parameters 2>/dev/null | awk 'NR > 2 { exit } ; /^hostname / { print $2 }')
-#	echo $hostname
-#}
-
 function rmssh () {
     sed -i "/$1/d" ~/.ssh/known_hosts
-}
-
-susu() {
-    command sudo -H -u ${1:-root} \
-        env ZDOTDIR=${ZDOTDIR:-$HOME} \
-            ZSH=$ZSH ${DISPLAY+DISPLAY=$DISPLAY} \
-            ${SSH_TTY+SSH_TTY=$SSH_TTY} \
-            ${SSH_AUTH_SOCK+SSH_AUTH_SOCK=$SSH_AUTH_SOCK} \
-            ${ZSH_NAME} -i -l
 }
 
 extract() {
@@ -63,24 +33,3 @@ extract() {
         echo "'$1' is not a valid file"
     fi
 }
-
-# Automatic pipenv shell activation/deactivation
-_togglePipenvShell() {
-    # deactivate shell if Pipfile doesn't exist and not in a subdir
-    if [[ ! -a "$PWD/Pipfile" ]]; then
-        if [[ "$PIPENV_ACTIVE" == 1 ]]; then
-            if [[ "$PWD" != "$pipfile_dir"* ]]; then
-                exit
-            fi
-        fi
-    fi
-
-    # activate the shell if Pipfile exists
-    if [[ "$PIPENV_ACTIVE" != 1 ]]; then
-        if [[ -a "$PWD/Pipfile" ]]; then
-            export pipfile_dir="$PWD"
-            pipenv shell
-        fi
-    fi
-}
-chpwd_functions+=(_togglePipenvShell)
