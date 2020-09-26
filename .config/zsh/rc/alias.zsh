2dfa7c73c57c731ed70405ccd481a28f618014cf
Jean-Philippe Menil <jpmenil@gmail.com>
2020-09-26 11:26:22 +0200

    alias: re-order
    Signed-off-by: Jean-Philippe Menil <jpmenil@gmail.com>

--- .config/zsh/rc/alias.zsh
+++ .config/zsh/rc/alias.zsh
@@ -11,34 +11,9 @@ alias gp='git push'
 alias grv='git remote -v'
 alias gst='git status'
 
-# rot13
-alias rot13="tr 'A-Za-z' 'N-ZA-Mn-za-m'"
 # ls
 alias ll="ls -la --color=auto"
 alias llrt="ls -lrta --color=auto"
-
-# xbps
-alias xq="xbps-query -Rs"
-alias xi="sudo xbps-install -S"
-alias xo="sudo xbps-remove -Oo"
-alias xu="sudo xbps-install -Su"
-
-alias x=extract
-
-alias config="git --git-dir=$HOME/WORK/jp/git/dotfiles/ --work-tree=$HOME"
-alias e="qemacs"
-alias xclip="xclip -selection clipboard"
-alias monip="curl ipv4.icanhazip.com"
-alias monip6="curl ipv6.icanhazip.com"
-alias pass="PASSWORD_STORE_DIR=~/WORK/jp/git/password-store/ pass"
-alias grep='grep --color=auto'
-alias sudo='sudo '
-alias notif='python ~/bin/pyrnotify.py 4321 & ssh -f -N -q -R 4321:localhost:4321 numenor'
-
-alias docker='sudo docker'
-alias rm_docker='docker ps -a | awk '\''{print $1}'\'' | grep -v CONTAINER | xargs docker rm'
-alias vlc='vlc --avcodec-hw=vaapi'
-alias cpu_perf='echo performance | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor'
 
 # tmux
 alias ta='tmux attach -t'
@@ -47,3 +22,27 @@ alias ts='tmux new-session -s'
 alias tl='tmux list-sessions'
 alias tksv='tmux kill-server'
 alias tkss='tmux kill-session -t'
+
+# xbps
+alias xq="xbps-query -Rs"
+alias xi="sudo xbps-install -S"
+alias xo="sudo xbps-remove -Oo"
+alias xu="sudo xbps-install -Su"
+
+# stuff
+alias abook="abook -C ~/.config/abook/abookrc --datafile ~/.config/abook/addressbook"
+alias config="git --git-dir=$HOME/WORK/jp/git/dotfiles/ --work-tree=$HOME"
+alias cpu_perf='echo performance | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor'
+alias docker='sudo docker'
+alias e="qemacs"
+alias grep='grep --color=auto'
+alias monip="curl ipv4.icanhazip.com"
+alias monip6="curl ipv6.icanhazip.com"
+alias notif='python ~/bin/pyrnotify.py 4321 & ssh -f -N -q -R 4321:localhost:4321 numenor'
+alias notmuch="notmuch --config ~/.config/notmuch-config"
+alias pass="PASSWORD_STORE_DIR=~/WORK/jp/git/password-store/ pass"
+alias rm_docker='docker ps -a | awk '\''{print $1}'\'' | grep -v CONTAINER | xargs docker rm'
+alias rot13="tr 'A-Za-z' 'N-ZA-Mn-za-m'"
+alias x=extract
+alias vlc='vlc --avcodec-hw=vaapi'
+alias xclip="xclip -selection clipboard"
