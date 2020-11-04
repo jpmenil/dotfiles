501a0be472d0dc84a1289acb4b59af014b4b4bee
Jean-Philippe Menil <jpmenil@gmail.com>
2020-11-04 15:39:35 +0100

    better music status bar
    Signed-off-by: Jean-Philippe Menil <jpmenil@gmail.com>

--- bin/xsetcmus.sh
+++ bin/xsetcmus.sh
@@ -4,15 +4,22 @@ if ps -C cmus > /dev/null; then
     substring="status playing"
     result=`cmus-remote -C status`
     logitech="Welcome to Logitech Media Server"
+    inter="franceinter"
 
-    if [ "$result" != "${result%$substring*}" ]; then
-        title=${result#*title}
+    if [ ! "${result##*"$substring"*}" ]; then
         stream=${result#*stream }
-        if [ "$result" = "${result%$logitech*}" ]; then
-            echo  Now Playing:${stream%%set*}-${title%%tag*}┊
-        else
-            echo  Now Playing: ${stream%%set*}┊
+        title=${result#*title }
+
+        artist_title=${stream%%set*}
+        stream_source=${title%%tag*}
+
+        if [ ! "${result##*"$inter"*}" ]; then
+            artist_title=""
+            stream_source="France Inter"
+        elif [ ! "${result##*"$logitech"*}" ]; then
+            stream_source=${title%%stream*}
         fi
+        echo  Now Playing: ${artist_title} -- ${stream_source}┊
     else
         echo " ";
     fi
