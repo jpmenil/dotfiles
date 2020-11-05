621b32d59859912e7a88f00aee1bd43bed02b36b
Jean-Philippe Menil <jpmenil@gmail.com>
2020-11-05 09:04:14 +0100

    mailboxes_sync: fix SC2045 & SC2086
    Signed-off-by: Jean-Philippe Menil <jpmenil@gmail.com>

--- bin/mailboxes_sync.sh
+++ bin/mailboxes_sync.sh
@@ -15,9 +15,9 @@ export DISPLAY=:0
 export XAUTHORITY="${HOME}/.Xauthority"
 
 # Loop through every account in ~/.mail
-for account in $(ls ~/.mail); do
+for account in ~/.mail/*; do
     # Find all unread mail whose file is newer that the last time this script was run and count them
-    newcount=$(find .mail/$account/Inbox/new -type f -newer ~/.config/neomutt/.mailsynclast 2> /dev/null | wc -l)
+    newcount=$(find .mail/"$account"/Inbox/new -type f -newer ~/.config/neomutt/.mailsynclast 2> /dev/null | wc -l)
     # Are there any new unread mail?
     if [ "$newcount" -gt "0" ]; then
         # Send a notification
