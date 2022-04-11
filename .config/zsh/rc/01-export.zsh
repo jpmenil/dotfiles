e0331b81533bc756d413cc8a2cebaaabf86f75d1
Jean-Philippe Menil <jpmenil@gmail.com>
2022-04-11 21:38:34 +0200

    zsh: use emacs for VISUAL
    Signed-off-by: Jean-Philippe Menil <jpmenil@gmail.com>

--- .config/zsh/rc/01-export.zsh
+++ .config/zsh/rc/01-export.zsh
@@ -4,9 +4,10 @@ export GOPATH="$HOME/go"
 export GREP_COLORS='ms=01;32:mc=01;31:sl=:cx=:fn=33:ln=32:bn=32:se=36'
 export LS_COLORS="di=0;36:ln=0;31:so=37:pi=1;35:ex=37:bd=37:cd=37:su=37:sg=37:tw=0;36:ow=0;36"
 export PAGER="less"
-export VISUAL="qe"
+export VISUAL="emacsclient -nw"
 export EDITOR=$VISUAL
 export XDG_CONFIG_HOME="$HOME/.config"
+export MOZ_X11_EGL=1
 
 # gpg & ssh
 if [ -z $SSH_TTY ]; then
