ab0de1b050b91783b02df4b0166695be346f2768
Jean-Philippe Menil <jpmenil@gmail.com>
2022-04-11 21:40:32 +0200

    zsh: handle macos in mfa function
    Signed-off-by: Jean-Philippe Menil <jpmenil@gmail.com>

--- .config/zsh/rc/functions.zsh
+++ .config/zsh/rc/functions.zsh
@@ -3,11 +3,21 @@
 # Functions
 
 function mfa () {
-    oathtool --base32 --totp "$(pass show $1/2fa)" | xclip -selection clipboard ;
+    case "$(uname -s)" in
+        Darwin)
+            oathtool --base32 --totp "$(pass show "$1"/2fa)" | pbcopy
+            ;;
+        Linux)
+            oathtool --base32 --totp "$(pass show "$1"/2fa)" | xclip -selection clipboard
+            ;;
+        *)
+            echo "Not available at this time"
+            ;;
+    esac
 }
 
 function fk() {
-    ps aux | grep $1 | grep -v grep | awk '{print $2}' | xargs kill -9
+    pgrep "$1" | awk '{print $2}' | xargs kill -9
 }
 
 function rmssh () {
@@ -17,20 +27,20 @@ function rmssh () {
 extract() {
     # Extract most know archives with one command
     # http://alias.sh/extract-most-know-archives-one-command
-    if [ -f $1 ] ; then
-        case $1 in
-            *.tar.bz2)   tar xjvf $1    ;;
-            *.tar.gz)    tar xzvf $1    ;;
-            *.tar.xz)    tar Jxvf $1    ;;
-            *.bz2)       bunzip2 $1     ;;
-            *.rar)       unrar e $1     ;;
-            *.gz)        gunzip $1      ;;
-            *.tar)       tar xvf $1      ;;
-            *.tbz2)      tar xjvf $1     ;;
-            *.tgz)       tar xzvf $1     ;;
-            *.zip)       unzip $1       ;;
-            *.Z)         uncompress $1  ;;
-            *.7z)        7z x $1        ;;
+    if [ -f "$1" ] ; then
+        case "$1" in
+            *.tar.bz2)   tar xjvf "$1"    ;;
+            *.tar.gz)    tar xzvf "$1"    ;;
+            *.tar.xz)    tar Jxvf "$1"    ;;
+            *.bz2)       bunzip2 "$1"     ;;
+            *.rar)       unrar e "$1"     ;;
+            *.gz)        gunzip "$1"      ;;
+            *.tar)       tar xvf "$1"      ;;
+            *.tbz2)      tar xjvf "$1"     ;;
+            *.tgz)       tar xzvf "$1"     ;;
+            *.zip)       unzip "$1"       ;;
+            *.Z)         uncompress "$1"  ;;
+            *.7z)        7z x "$1"        ;;
             *)     echo "'$1' cannot be extracted via extract()" ;;
         esac
     else
@@ -41,7 +51,7 @@ extract() {
 # Check if main exists and use instead of master
 function git_main_branch() {
     local branch
-    for branch in main trunk; do
+    for branch in develop main trunk; do
         if command git show-ref -q --verify refs/heads/$branch; then
             echo $branch
             return
